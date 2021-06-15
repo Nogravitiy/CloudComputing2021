38c1177a7722105b610f728cbaa5171192b4ffc4
Nogravitiy <58252937+Nogravitiy@users.noreply.github.com>
2021-06-16 00:24:26 +0200

    Update unique_url.test.js

--- test/unique_url.test.js
+++ test/unique_url.test.js
@@ -3,6 +3,9 @@ const expect = require('chai').expect;
 const NodeCache = require( "node-cache" );
 const myCache = new NodeCache();
 
+var readline = require('readline');
+var fs = require('fs');
+
 const Url = require('../url.model');
 
 const shorturl = require('../url.controller');
@@ -11,21 +14,27 @@ describe('unique_url tests', () => {
     describe('url.controller.createshortLink() Test', () => {
         it('should be unique', () => {
 
-            let names = [];
+            let names = [] ;
 
-            for(let i=0; i<1000; i++){
+            var myInterface = readline.createInterface({
+                input: fs.createReadStream('./test/urlList.txt')
+              });
+              
+              var lineno = 0;
+              myInterface.on('line', function (line) {
+                lineno++;
                 let unique_name = shorturl.shortLink_create();
-
                 if(!names.includes(unique_name)){
                     names.push(unique_name);
                 }else {
                     throw new Error('sorry, unique_name already exists :(' );
                 }
-            }
+              });
 
         });
     });
 }) 
+
 
 
 
