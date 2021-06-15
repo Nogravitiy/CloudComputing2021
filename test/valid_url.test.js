8e6b1503b123212c0a236e1d3db26f942f3687bc
Nogravitiy <58252937+Nogravitiy@users.noreply.github.com>
2021-06-16 00:24:54 +0200

    Update valid_url.test.js

--- test/valid_url.test.js
+++ test/valid_url.test.js
@@ -4,17 +4,22 @@ const shorturl = require('../url.controller');
 
 
 describe('valid_url test', () => {
-    it('test urls', async () => {
-        if(shorturl.checkUrl("google.de")) { throw new Error('Wrong result 0')};
-        if(shorturl.checkUrl("hp://google.de")) { throw new Error('Wrong result 1')};
-        if(shorturl.checkUrl("http//google.de")) { throw new Error('Wrong result 2')};
-        if(shorturl.checkUrl("http:/google.de")) { throw new Error('Wrong result 3')};
-        if(shorturl.checkUrl("http:google.de")) { throw new Error('Wrong result 4')};
-        if(!shorturl.checkUrl("http://google.de")) { throw new Error('Wrong result 5')};
-        if(!shorturl.checkUrl("https://google.de")) { throw new Error('Wrong result 6')};
-        if(!shorturl.checkUrl("https://github.com/Nogravitiy/CloudComputing2021")) { throw new Error('Wrong result 7')};
-        if(!shorturl.checkUrl("https://saml.dhbw-stuttgart.de/idp/profile/SAML2/Redirect/SSO?execution=e1s1")) { throw new Error('Wrong result 8')};
-        if(!shorturl.checkUrl("https://dualis.dhbw.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=COURSERESULTS&ARGUMENTS=-N999294960049353,-N000307,")) { throw new Error('Wrong result 9')};
-    })
+    it('valid url 1', async () => {
+        if(!shorturl.checkUrl("http://google.de")) { throw new Error('Wrong result 1')};
+    });
+    it('valid url 2', async () => {
+        if(!shorturl.checkUrl("https://google.de")) { throw new Error('Wrong result 2')};
+    });
+    it('valid url 3', async () => {
+        if(!shorturl.checkUrl("https://github.com/Nogravitiy/CloudComputing2021")) { throw new Error('Wrong result 3')};
+    });
+    it('valid url 4', async () => {
+        if(!shorturl.checkUrl("https://saml.dhbw-stuttgart.de/idp/profile/SAML2/Redirect/SSO?execution=e1s1")) { throw new Error('Wrong result 4')};
+    });
+    it('valid url 5', async () => {
+        if(!shorturl.checkUrl("https://dualis.dhbw.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=COURSERESULTS&ARGUMENTS=-N999294960049353,-N000307,")) { throw new Error('Wrong result 5')};
+    });
 })
+
+
 
